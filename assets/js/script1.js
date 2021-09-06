c13a7c15f4f1b5fba0f6662d45501a3815902353
Karrthik Arya <200020068@iitb.ac.in>
2021-09-06 17:59:17 +0530

    navbar corrected

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -2,6 +2,37 @@
 (function ($) {
     'use strict';
 
+    
+    // Sticky Menu
+    $(window).scroll(function () {
+        if ($('.navigation').offset().top > 100) {
+            $('.navigation').addClass('nav-bg');
+        } else {
+            $('.navigation').removeClass('nav-bg');
+        }
+    });
+
+    // Background-images
+    $('[data-background]').each(function () {
+        $(this).css({
+            'background-image': 'url(' + $(this).data('background') + ')'
+        });
+    });
+
+    // background color
+    $('[data-color]').each(function () {
+        $(this).css({
+            'background-color': $(this).data('color')
+        });
+    });
+
+    // progress bar
+    $('[data-progress]').each(function () {
+        $(this).css({
+            'bottom': $(this).data('progress')
+        });
+    });
+
     consoleText(["Web and Coding Club <br> <font color = '#ffa500'>IIT Bombay</font>"], 'text',['tomato']);
     let active = false;
     teamText(['<font color = "lightgreen">WnCC@itc:</font><font color = "blue">~</font>$ ./WnCC-team.sh <br> \
@@ -245,37 +276,6 @@ function teamText(words, id, colors) {
   }, 30)
   
 }
-    // Sticky Menu
-    $(window).scroll(function () {
-        if ($('.navigation').offset().top > 100) {
-            $('.navigation').addClass('nav-bg');
-        } else {
-            $('.navigation').removeClass('nav-bg');
-        }
-    });
-
-    // Background-images
-    $('[data-background]').each(function () {
-        $(this).css({
-            'background-image': 'url(' + $(this).data('background') + ')'
-        });
-    });
-
-    // background color
-    $('[data-color]').each(function () {
-        $(this).css({
-            'background-color': $(this).data('color')
-        });
-    });
-
-    // progress bar
-    $('[data-progress]').each(function () {
-        $(this).css({
-            'bottom': $(this).data('progress')
-        });
-    });
-
-
     /* ########################################### hero parallax ############################################## */
     window.onload = function () {
 
@@ -433,7 +433,7 @@ function teamText(words, id, colors) {
         }
     });
     
-
+    
 
     
 })(jQuery);
