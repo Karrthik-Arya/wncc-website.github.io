ecfd1f63977635bd9dbe43ce36763c43a388c740
Karrthik Arya <200020068@iitb.ac.in>
2021-09-20 14:37:41 +0530

    minor corrections

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -87,7 +87,7 @@
      Setting up dependency tree <font color = "tomato">tty14</font> <br> \
      Fetched 260 kb in 1s(286 kb/s) <br> \
      Preparing to unpack     .          .           .  <font color = "lightgreen">/managers.gz</font> <br> \
-     Adding "Convenrs to <font color = "blue">/usr/bin/tty14/members</font>" <br>\
+     Adding "Conveners to <font color = "blue">/usr/bin/tty14/members</font>" <br>\
      Build Successful!<br>\
      <font color = "green">Click on the images to know about the members</font>'],
      ["teamtext"], ["aliceblue"]);
